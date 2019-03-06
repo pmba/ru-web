86fec380e672008a4a475b67cbd44f48313f4397
Lucas <lucasomena@hotmail.com>
2019-03-06 15:32:22 -0300

    todo

--- controllers/routes/auth.js
+++ controllers/routes/auth.js
@@ -22,26 +22,51 @@ router.post('/registrar', middleware.validation, (req, res) => {
             errors: errors
         });
     } else {
+        /*
+        TODO:
+          if (it's a valid cpf)
+          {
+              if (cpf is in database)
+              {
+                  if (password match)
+                  {
+                      ... login
+                  }
+                  else
+                  {
+                      try again
+                  }
+              }
+              else
+              {
+                puppeteer...
+              }
+          }
+          else
+          {
+              return error
+          }
+        */
         (async () => {
             const browser = await puppeteer.launch();
-    
+
             const page = await browser.newPage();
             await page.goto('https://sistemas.ufal.br/academico/login.seam');
-    
+
             await page.type('[id="loginForm:username"]', req.body.username);
             await page.type('[id="loginForm:password"]', req.body.password);
             await page.click('[id="loginForm:entrar"]');
-    
+
             var url = page.url().split('?');
-    
+
             if (url.length > 1) {
                 var cid = url[1].split('=');
-    
+
                 await page.goto(`https://sistemas.ufal.br/academico/matricula/visualizar.seam?cid=${cid[1]}`);
                 await page.screenshot({
                     path: 'screenshot.png'
                 });
-    
+
                 await browser.close();
 
                 var newUser = new User({
@@ -65,7 +90,7 @@ router.post('/registrar', middleware.validation, (req, res) => {
                     error: 'user does not exists'
                 });
             }
-    
+
         })();
     }
 });
