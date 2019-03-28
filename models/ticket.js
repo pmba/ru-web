8b89f01df5c883df9d3718ea1b1bb40c832fc00e
Lucas <lucasomena@hotmail.com>
2019-03-28 08:57:42 -0300

    model adapt and new functions

--- models/ticket.js
+++ models/ticket.js
@@ -1,5 +1,7 @@
 var mongoose = module.require('mongoose');
 
+const DishSchema = module.require('./dish').Schema;
+
 var TicketSchema = new mongoose.Schema({
     user_info: {
         name        : String,
@@ -12,7 +14,52 @@ var TicketSchema = new mongoose.Schema({
     date: {
         type   : Date,
         default: Date.now
+    },
+    validatedStatus: {
+        type: Boolean,
+        default: false
+    },
+    validateDate: {
+        type : Date,
+        default: null
+    },
+    validatedDishs: {
+        type: [DishSchema],
+        default: []
+    },
+    validateRating: {
+        type: Boolean,
+        default: false
+    },
+    ratingComment: {
+        type: String,
+        default: null
     }
+    //TODO: separar os objetos em :  (nao consegui usar eles dessa forma na funcao updateOne())
+  /*  validate: {
+        status: {
+          type: Boolean,
+          default: false
+        },
+        date: {
+          type: Date,
+          default: null
+        },
+        dishs: {
+          type: [DishSchema],
+          default: []
+        }
+    }
+    rating:{
+      status: {
+        type: Boolean,
+        default: false
+      },
+      comment: {
+        type: String,
+        default: null
+      }
+    } */
 });
 
 var Ticket = module.exports = mongoose.model('Ticket', TicketSchema);
@@ -23,6 +70,23 @@ module.exports.createTicket = (newTicket, callback) => {
 
 module.exports.getById = (ticketID, callback) => {
     Ticket.findById(ticketID, callback);
+}
+
+//Validar quando o usuário usar o ticket
+module.exports.validateTicket = (ticketID, dishs, callback) => {
+    Ticket.updateOne({ _id: ticketID }, {
+        validatedStatus: true,
+        validateDate: Date.now(),
+        validatedDishs: dishs
+    }, callback);
+}
+
+//Validar quando o usuário avaliar os pratos do ticket
+module.exports.validateRating = (ticketID, comment, callback) => {
+    Ticket.updateOne({ _id: ticketID }, {
+        validateRating: true,
+        ratingComment: comment
+    }, callback);
 }
 
 module.exports.getByUserID = (userID, callback) => {
