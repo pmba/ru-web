ffe1909f283c24804e06598727297963d1c2f3a9
Phyllipe Bezerra <pmba@ic.ufal.br>
2019-04-01 10:20:39 -0300

    add missing .js to admin profile page

--- public/js/admin-profile.js
+++ public/js/admin-profile.js
@@ -1,3 +1,5 @@
+var menuOpened = false;
+
 $(document).ready(function () {
     $('.table').DataTable({
         "language": {
@@ -24,6 +26,22 @@ $(document).ready(function () {
             }
         }
     });
+});
+
+$('#toggleMenuBtn').on('click', () => {
+    if (!menuOpened) {
+        $('#navOptions').css('height', '100%');
+        $('.btn-menu').css({
+            'transform': 'scaleY(-1)'
+        });
+        menuOpened = !menuOpened;
+    } else {
+        $('#navOptions').css('height', '0');
+        $('.btn-menu').css({
+            'transform': 'scaleY(1)'
+        });
+        menuOpened = !menuOpened;
+    }
 });
 
 function proceedToDelete(deleteURL) {
